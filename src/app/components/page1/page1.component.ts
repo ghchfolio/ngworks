b027ce6a8a316f5c5318e80c0eb420261e420c49
CH Folio <ghchfolio@gmail.com>
2024-02-02 13:44:55 -0600

    add image

--- src/app/components/page1/page1.component.ts
+++ src/app/components/page1/page1.component.ts
@@ -10,6 +10,10 @@ import { Component } from '@angular/core';
     <li>copied code from angular site, paste in notepad then paste in editor</li>
     <li>remove imported files, when error appears re-import</li>
    </ul>
+
+   <div class="text-center">
+    <img src="assets/icons/icon-72x72.png" alt="angular logo" title="angular logo" width="72"  height="72"/>
+   </div>
   `,
     styles: ``
 })
