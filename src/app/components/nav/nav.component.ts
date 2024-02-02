191951bbe17381e291f535b0e99a4cc5031dc80d
CH Folio <ghchfolio@gmail.com>
2024-02-02 13:39:37 -0600

    update nav color

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -6,10 +6,10 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
     standalone: true,
     imports: [RouterLink, RouterLinkActive, NavComponent],
     template: `
-    <nav class="container navbar navbar-expand-lg bg-body-tertiary">
+    <nav class="container navbar navbar-expand-lg bg-primary" data-bs-theme="dark">
   <div class="container-fluid">
     
-  <a class="navbar-brand" href="#">Test 00</a>
+  <a class="navbar-brand" href="#">Test 01</a>
 
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
