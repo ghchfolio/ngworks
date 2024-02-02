d7ad6cdf40f895bb68a6be29174a8f4bea2a6f55
CH Folio <ghchfolio@gmail.com>
2024-02-02 13:39:53 -0600

    add some copy

--- src/app/components/page1/page1.component.ts
+++ src/app/components/page1/page1.component.ts
@@ -1,15 +1,17 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-page1',
-  standalone: true,
-  imports: [],
-  template: `
-    <p>
-      page1 works!
-    </p>
+    selector: 'app-page1',
+    standalone: true,
+    imports: [],
+    template: `
+   <h6>Notes</h6>
+   <ul>
+    <li>copied code from angular site, paste in notepad then paste in editor</li>
+    <li>remove imported files, when error appears re-import</li>
+   </ul>
   `,
-  styles: ``
+    styles: ``
 })
 export class Page1Component {
 
--- src/app/components/page2/page2.component.ts
+++ src/app/components/page2/page2.component.ts
@@ -1,15 +1,17 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-page2',
-  standalone: true,
-  imports: [],
-  template: `
-    <p>
-      page2 works!
-    </p>
+    selector: 'app-page2',
+    standalone: true,
+    imports: [],
+    template: `
+    <h6>Helpful Links</h6>
+
+    <ul>
+        <li><a href="https://angular.io/guide/service-worker-communications" target="_blank" rel="noopener noreferrer">Service worker communications</a></li>
+    </ul>
   `,
-  styles: ``
+    styles: ``
 })
 export class Page2Component {
 
